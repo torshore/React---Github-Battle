583930013ce696c164254521172c8e46e3bdd8e7
torshore <toryshoreman@gmail.com>
2017-04-24 10:44:18 -0400

    Popular

---
+++ app/components/Popular.js
@@ -0,0 +1,13 @@
+import React from 'react-dom';
+
+export default class Popular extends Component {
+  render() {
+    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <div>
+        Popular! 
+      </div>
+    )
+  }
+}
---
+++ app/components/app.js
@@ -0,0 +1,12 @@
+import 'React' from 'react-dom';
+import Popular from './Popular';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+      Popular!
+      </div>
+    )
+  }
+}
